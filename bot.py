29838c7246ae901d20909bdddf09537b4f72e36c
VoltaireNoir <mzahmed95@gmail.com>
2022-09-20 18:27:21 +0530

    Upgrade to v20, add coin toss, get_id commands

--- bot.py
+++ bot.py
@@ -1,44 +1,43 @@
-#!/usr/bin/env python3
-import logging
-import commands
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+import commands, logging
 
 # Enable logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-
 logger = logging.getLogger(__name__)
 
+# Define a few command handlers. These usually take the two arguments update and
+# context.
 
-def message_handler(update: Update, context: CallbackContext) -> None:
+async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     pass
 
-
 def main() -> None:
-    """Start the bot."""
+    # Read token from file
     try:
         with open("token") as f:
             TOKEN = f.readline().strip()
     except FileNotFoundError:
-        print("Token file not found. Exiting.")
+        logger.critical("Token file not found. Exiting.")
         return
 
-    updater = Updater(TOKEN)
-    dispatcher = updater.dispatcher
+    # Create application using token
+    application = Application.builder().token(TOKEN).build()
 
-    # Add commands
-    dispatcher.add_handler(CommandHandler("start", commands.start))
-    dispatcher.add_handler(CommandHandler("help", commands.help))
+    # Add command handlers
+    application.add_handler(CommandHandler("start", commands.start))
+    application.add_handler(CommandHandler("help", commands.help))
+    application.add_handler(CommandHandler("toss", commands.coin_toss))
+
+    #application.add_handler(CommandHandler("id", commands.get_id))
 
     # Add message handler
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, message_handler))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
 
-    # Start the Bot
-    updater.start_polling()
-    updater.idle()
+    # Run the bot
+    application.run_polling()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- commands.py
+++ commands.py
@@ -1,20 +1,46 @@
 #!/usr/bin/env python3
-from telegram import ForceReply, Update
-from telegram.ext import CallbackContext
+from telegram import ForceReply, Update, Animation, Sticker
+from telegram.ext import ContextTypes
+import random, asyncio, logging
 
 # All command functions go here, unless their implementation is too large
 # then they should be placed in their .py files and imported here for ease of use
 
-def start(update: Update, context: CallbackContext) -> None:
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
         reply_markup=ForceReply(selective=True),
     )
 
-
-def help(update: Update, context: CallbackContext) -> None:
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
-    update.message.reply_text("Uh, I don't know how I could help you!")
+    await update.message.reply_text("Uh oh, I don't know how I could help you!")
 
+
+async def coin_toss(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logging.info("Coin toss requested")
+    # IDs for media stored on Telegram's server
+    LOADING_GIF_ID = "CgACAgUAAxkDAAMUYxWtM8zT6CEQFzs5mVodW66_qjYAAmIGAALAKrBU7V09eSPo2b0pBA", "AgADagYAAsAqsFQ"
+    HEADS_ID = "CAACAgUAAxkDAAN9YxXJh1HRWj9iW3Bicn6P-xOpZ3YAArAGAALAKrBUAb9ML3lUOEIpBA", "AgADsAYAAsAqsFQ"
+    TAILS_ID = "CAACAgUAAxkDAAN_YxXJz-PXVEDa_2G1U36f3JJv-7QAArEGAALAKrBUb-oCzAago38pBA", "AgADsQYAAsAqsFQ"
+
+    loading_gif = Animation(LOADING_GIF_ID[0],LOADING_GIF_ID[1],638,544,3.3)
+    heads_sticker = Sticker(HEADS_ID[0],HEADS_ID[1],512,512,False,False,"REGULAR")
+    tails_sticker = Sticker(TAILS_ID[0],TAILS_ID[1],512,512,False,False,"REGULAR")
+
+    loading = await update.message.reply_animation(loading_gif,duration=3.3)
+    await asyncio.sleep(3.3)
+    await loading.delete()
+    await update.message.reply_sticker(
+        random.choice([heads_sticker,tails_sticker]))
+    logging.info("Coint toss complete")
+
+# Send media to upload to Telegram servers and retreive ID to reuse in the future
+async def get_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    file = "media/CoinTailsT.png"
+    with open(file,"rb") as f:
+        msg = await update.message.reply_sticker(f)
+    print(msg.sticker.file_id)
+    print(msg.sticker.file_unique_id)
